1df17826d7e674b04a50d02de8da8e7041b7226e
Zoe Xanos <zxanos@gmail.com>
2020-05-09 17:49:07 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -33,7 +33,7 @@ end
 row = 3
 column = 5
 matrix_stuff = [ORGANIC_PRODUCE, CONVENTIONAL_PRODUCE]
-matrix_lookup(matrix_stuff, row, column)
+puts matrix_lookup(matrix_stuff, row, column)
 
 def matrix_lookup(matrix, row, column)
   matrix[row][column]
